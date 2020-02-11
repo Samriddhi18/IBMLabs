1f328e8afce458cacc5c10517a68d13359061fca
Samriddhi18 <53456833+Samriddhi18@users.noreply.github.com>
2020-02-11 12:02:56 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter a number: '))
+sum = float(num1) + float(num2) #add two numbers
+print('Sum of {0} and {1} is {2}'.format(num1,num2,sum))
+sub = num1-num2 #subtract two numbers
+print('Subtraction of {0} and {1} is {2}'.format(num1,num2,sub))
+mul = num1*num2 #multiply two numbers
+print('Muliplication of {0} and {1} is {2}'.format(num1,num2,mul))
+div = num1/num2 #division of two numbers
+print('Division of {0} and {1} is {2}'.format(num1,num2,div))
